c160bb61e258b9efbaf1f0d7c94cd9fed47d6eb4
Pranav9347 <nadgirpranav624@gmail.com>
2024-09-15 18:56:59 +0530

    Implement Faculty-registration
    Added post-request handling by the faculty's registration input data

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
@@ -11,11 +12,19 @@ const __dirname = path.dirname(__filename);
 // Serve static files from the Frontend directory
 app.use(express.static(path.join(__dirname, '../Frontend')));
 
+//mount the body-parser module:
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // Route for the home page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../Frontend', 'index.html'));
   });
-  
+
+//User registration
+app.post("/sign-up",(req,res)=>{
+  console.log(req.body);
+  res.sendStatus(200);
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
